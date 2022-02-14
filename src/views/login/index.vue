06710d2131dd88d7be179160df504f69c4b92b75
Liqiuyue9597 <875731429@qq.com>
2022-02-15 00:06:14 +0800

    "svg"

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -17,9 +17,7 @@
       <!-- password -->
       <el-form-item>
         <span class="svg-container">
-          <el-icon>
-            <avatar />
-          </el-icon>
+          <svg-icon icon="https://res.lgdsunday.club/user.svg"></svg-icon>
         </span>
         <el-input placeholder="password" name="password" type="text"></el-input>
         <span class="show-pwd">
@@ -39,6 +37,7 @@
 
 <script setup>
 import { Avatar } from '@element-plus/icons'
+import SvgIcon from '@/components/SvgIcon/index.vue'
 </script>
 
 <style lang="scss" scoped>
@@ -49,14 +48,64 @@ $cursor: #fff;
 
 .login-container {
   min-height: 100%;
-  width:100%;
+  width: 100%;
   background-color: $bg;
   overflow: hidden;
 
   .login-form {
     position: relative;
     width: 520px;
-    max-width: 100%;
+    padding: 160px 35px 0;
+    margin: 0 auto;
+    overflow: hidden;
+
+    ::v-deep .el-form-item {
+      border: 1px solid rgba(255, 255, 255, 0.1);
+      border-radius: 5px;
+      color: #454545;
+    }
+
+    ::v-deep .el-input {
+      display: inline-block;
+      height: 47px;
+      width: 85%;
+
+      input {
+        background: transparent;
+        border: none;
+        border-radius: 0;
+        padding: 12px 5px 12px 5px;
+        color: $light_gray;
+        caret-color: $cursor;
+      }
+    }
+  }
+
+  .svg-container {
+    padding: 6px 5px 6px 15px;
+    color: $dark_gray;
+    vertical-align: middle;
+    display: inline-block;
+  }
+
+  .title-container {
+    position: relative;
+    .title {
+      font-size: 26px;
+      color: $light_gray;
+      margin: 0px auto 40px auto;
+      text-align: center;
+      font-weight: bold;
+    }
+  }
+  .show-pwd {
+    position: absolute;
+    right: 10px;
+    top: 7px;
+    font-size: 16px;
+    color: $dark_gray;
+    cursor: pointer;
+    user-select: none;
   }
 }
 </style>
