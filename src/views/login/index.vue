56cf7aba380b97d0e534887662686e4ab5cf0031
Liqiuyue9597 <875731429@qq.com>
2022-02-14 11:09:24 +0800

    login

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,13 +1,62 @@
 <template>
   <div class="login-container">
     <el-form class="login-form">
-      <div class="title-container"></div>
+      <div class="title-container">
+        <h3 class="title">用户登录</h3>
+      </div>
+      <!-- username -->
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="username" name="username" type="text"></el-input>
+      </el-form-item>
+
+      <!-- password -->
+      <el-form-item>
+        <span class="svg-container">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+        <el-input placeholder="password" name="password" type="text"></el-input>
+        <span class="show-pwd">
+          <el-icon>
+            <avatar />
+          </el-icon>
+        </span>
+      </el-form-item>
+
+      <!-- denglu -->
+      <el-button type="primary" style="width: 100%; margin-bottom: 30px"
+        >登录</el-button
+      >
     </el-form>
   </div>
 </template>
 
-<script>
-export default {
-
-}
+<script setup>
+import { Avatar } from '@element-plus/icons'
 </script>
+
+<style lang="scss" scoped>
+$bg: #2d3a4b;
+$dark_gray: #889aa4;
+$light_gray: #eee;
+$cursor: #fff;
+
+.login-container {
+  min-height: 100%;
+  width:100%;
+  background-color: $bg;
+  overflow: hidden;
+
+  .login-form {
+    position: relative;
+    width: 520px;
+    max-width: 100%;
+  }
+}
+</style>
